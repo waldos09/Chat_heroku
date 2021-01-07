61301809b599353e1a4d102d690c546ffa63dfb7
waldos09 <legos567@gmail.com>
2021-01-07 15:15:39 -0600

    herokuv2.3

--- Node_heroku/routes/index.js
+++ Node_heroku/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router;
 
-router.get('index', (req, res => {
+router.get('/', (req, res => {
     res.render('../public/index.php');
 }));
