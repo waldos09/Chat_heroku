f71df4752dc4d60c1acb19413d709c631ae526db
waldos09 <legos567@gmail.com>
2021-01-07 15:36:37 -0600

    herokuv2.6

--- Node_heroku/index.js
+++ Node_heroku/index.js
@@ -12,14 +12,10 @@ const server = app.listen(app.get('port'), () => {
     console.log("el servidor esta en el puerto ", app.get('port'));
 });
 
-//routes
-const indexRouters = require('./routes/index');
 
 //enviar archivos estaticos (public)
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/', indexRouters);
 //cifrado rsa
 const key = new NodeRSA({ b: 1024 });
 //var encryptedString = key.encrypt(str, 'base64');
